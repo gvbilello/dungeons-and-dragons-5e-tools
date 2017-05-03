927f2dc4e7242d87f78ff47167249ec692f1b0f8
Gregory V. Bilello <gregory.bilello@gmail.com>
2017-05-03 15:23:37 -0400

    All seed data added and working as intended, all models and associations working as intended.

--- db/migrate/20170503181739_create_creatures.rb
+++ db/migrate/20170503181739_create_creatures.rb
@@ -1,6 +1,7 @@
 class CreateCreatures < ActiveRecord::Migration[5.0]
   def change
     create_table :creatures do |t|
+    	# general information
    		t.string :name, null: false
    		t.string :size
    		t.string :creature_type
@@ -10,17 +11,35 @@ class CreateCreatures < ActiveRecord::Migration[5.0]
    		t.integer :hit_points
    		t.string :hit_dice
    		t.string :speed
+   		# main stats
    		t.integer :strength
    		t.integer :dexterity
    		t.integer :constitution
    		t.integer :intelligence
    		t.integer :wisdom
    		t.integer :charisma
+   		# saving throws
    		t.integer :constitution_save
    		t.integer :intelligence_save
    		t.integer :wisdom_save
+   		# skills
+   		t.integer :athletics
+   		t.integer :acrobatics
+   		t.integer :stealth
+   		t.integer :arcana
    		t.integer :history
+   		t.integer :investigation
+   		t.integer :nature
+   		t.integer :religion
+   		t.integer :insight
+   		t.integer :medicine
    		t.integer :perception
+   		t.integer :survival
+   		t.integer :deception
+   		t.integer :intimidation
+   		t.integer :performance
+   		t.integer :persuasion
+   		# damage modifiers and other
    		t.string :damage_vulnerabilities
    		t.string :damage_resistances
    		t.string :damage_immunities
