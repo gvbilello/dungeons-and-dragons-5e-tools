2d253c8248e1383b3c4ec731205f1b8d86105e96
Gregory V. Bilello <gregory.bilello@gmail.com>
2017-05-05 18:28:52 -0400

    Add functions to determine if a creature has skills, damage modifiers, or saving throws - in order to correctly display information on Creature show page.

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -2,4 +2,16 @@ class Creature < ApplicationRecord
 	has_many :special_abilities
 	has_many :actions
 	has_many :legendary_actions
+
+	def has_skills?
+		return true if self.athletics || self.acrobatics || self.stealth || self.arcana || self.history || self.investigation || self.nature || self.religion || self.insight || self.medicine || self.perception || self.survival || self.deception || self.intimidation || self.performance || self.persuasion
+	end
+
+	def has_saving_throws?
+		return true if self.constitution_save || self.intelligence_save || self.wisdom_save
+	end
+
+	def has_damage_modifiers?
+		return true if self.damage_vulnerabilities.length > 0 || self.damage_resistances.length > 0 || self.damage_immunities.length > 0 || self.condition_immunities.length > 0
+	end
 end
